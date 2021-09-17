ed66c9434179ae87abdfa560c7e8331e772acf64
Mariano Rivera <marianorivera@Marianos-Air.hsd1.il.comcast.net>
2021-09-16 21:47:38 -0500

    pulling data from csv

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,22 +4,23 @@ import csv
 
 import statistics 
 
-csvpath= os.path.join('..', 'Resources', 'budget_data.csv')
-with open(csvpath) as csvfile:
+csvpath= os.path.join('Resources', 'budget_data.csv')
+
+count=0
+month=[]
+revenue=[]
+revenue_change=[]
+monthly_change=[]
+
+with open(csvpath,newline="") as csvfile:
     csvreader=csv.reader(csvfile, delimiter=',')
     print (csvreader)
-    csv_header=next (csvreader)
-    month= []
-    revenue=[]
-    revenue_change=[]
-    monthly_change=[]
-
- #Read the header row first
-    csv_header= next (csvreader)
+    csv_header= next(csvreader)
     print (f"CSV Header: {csv_header}")
 
 #Read each row of data after the header. Append.
     for row in csvreader:
+        count = count + 1
         month.append(row[0])
         revenue.append(row[1])
     print(len(month))
@@ -30,12 +31,12 @@ with open(csvpath) as csvfile:
     print (total_revenue)
 
     i=0
-    for i in range (len(revenue)-1):
+    for i in range(len(revenue)-1):
         profit_loss=int(revenue[i+1])- int (revenue[i])
         revenue_change.append(profit_loss)
     Total= sum(revenue_change)
     monthly_change=Total/len(revenue_change)
-    print(monthly_change)
+    print(revenue_change)
 
 #Greatest Increase & Decrease
     profit_increase=max (revenue_change)
@@ -47,6 +48,8 @@ with open(csvpath) as csvfile:
     month_increase=month [k+1]
     month_decrease=month [j+1]
 
+    average_change= round(sum(revenue_change)/len(revenue_change),2)
+
 #Analysis Statements
     output_path='Financial Analysis.txt'
 with open(output_path, 'w', newline='') as csvfile:
@@ -55,7 +58,15 @@ with open(output_path, 'w', newline='') as csvfile:
     csvwriter.writerow(["-----------------------------------"])
     csvwriter.writerow([f'Total Months: {len(month)}'])
     csvwriter.writerow([f'Total Profits/Losses: ${total_revenue}'])
-    csvwriter.writerow([f'Average Change: ${round(sum(monthly_change)/len(monthly_change),2)}'])
-    csvwriter.writerow([f'The Greatest Increase In Profits: ${monthly_change[0]}'])
-    csvwriter.writerow([f'The Greatest Decrease In Profits: ${monthly_change[len(monthly_change)-1]}'])
+    csvwriter.writerow([f'Average Change: ${average_change}'])
+    csvwriter.writerow([f'The Greatest Increase In Profits: {month_increase} (${profit_increase})'])
+    csvwriter.writerow([f'The Greatest Decrease In Profits: {month_decrease} (${profit_decrease})'])
 
+#Print Statements
+print("Financial Analysis")
+print("----------------------")
+print(f"Total Months: {len(month)}")
+print(f"Total Profits/Losses: ${total_revenue}")
+print(f'Average Change: ${average_change}')
+print(f'The Greatest Increase In Profits: {month_increase} (${profit_increase})')
+print(f'The Greatest Decrease In Profits: {month_decrease} (${profit_decrease})')
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -4,21 +4,22 @@ import csv
 
 #List variables
 candidates = []
-votes=[]
+votes={}
 
 #Count Votes
 total_votes=0
 
-election_csv= os.path.join('..', 'Resources', 'election_data.csv')
+election_csv= os.path.join('Resources', 'election_data.csv')
 with open(election_csv) as csvfile:
     csvreader=csv.reader(csvfile, delimiter= ',')
     next (csvreader, None)
     for row in csvreader:
-        candidate = row [2]
+        candidate = row[2]
         if candidate not in votes:
-            votes [candidate]=1
+            votes[candidate]=1
         else:
-            votes [candidate]+=1
+            votes[candidate]+=1
+        total_votes+=1
 
 results= []
 results.append("Election Results\n-------------------")
@@ -35,10 +36,20 @@ print (f"---------------------------")
 print (f"Winner: {max(votes, key=votes.get)}")
 print (f"---------------------------")
 
-#Text File
-filename= 'Results.txt'
-with open (filename, 'w') as file:
-    for result in results:
-        print (result)
-        file.write (result+ '\n')
-        
+#.txtFile
+output_path= election_csv
+
+write_electionCSV = f"{output_path}resultstext"
+
+filewriter = open(write_electionCSV, mode = 'w')
+
+filewriter.write("Election Results\n")
+filewriter.write("-----------------------\n")
+filewriter.write(f"Total Votes: {total_votes}\n")
+for key, value in votes.items():
+    filewriter.write(f"{key}: {value/total_votes: 0.3%} ({value})\n")
+filewriter.write("-----------------------\n")
+filewriter.write(f"Winner: {max(votes, key=votes.get)}\n")
+filewriter.write("-----------------------\n")
+
+filewriter.close()
